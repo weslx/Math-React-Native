47cff899da2c19d059c186af5133458d2a113cae
weslx <80607779+weslx@users.noreply.github.com>
2023-10-10 22:00:25 -0300

    Revert "+"
    This reverts commit 4750722a023422ba0812a63a296764d925553eec.

--- src/MedioTela.js
+++ src/MedioTela.js
@@ -1,62 +1,43 @@
-import React from "react";
-import { View, Text, Switch } from "react-native";
+import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import colors from '../colors';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function Settings() {
-  const [isNotificationsEnabled, setIsNotificationsEnabled] =
-    React.useState(false);
-  const [isPinEnabled, setIsPinEnabled] = React.useState(false);
+const Stack = createStackNavigator();
+
+const MedioTela = () => {
+  const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>Configurações</Text>
-      <View>
-        <Text>Notificações</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isNotificationsEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={setIsNotificationsEnabled}
-          value={isNotificationsEnabled}
-        />
-      </View>
-      <View>
-        <Text>PIN</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isPinEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={setIsPinEnabled}
-          value={isPinEnabled}
-        />
-      </View>
-      <Text
-        onPress={() => {
-          /* navigate to profile */
-        }}
-      >
-        Perfil
-      </Text>
-      <Text
-        onPress={() => {
-          /* navigate to privacy terms */
-        }}
-      >
-        Termos de privacidade
-      </Text>
-      <Text
-        onPress={() => {
-          /* navigate to feedback */
-        }}
-      >
-        Feedback
-      </Text>
-      <Text
-        onPress={() => {
-          /* reset settings */
-        }}
-      >
-        Restar configurações
-      </Text>
+    <View style={styles.center}>
+      <Button
+        title="1° Ano Ensino Médio"
+        color={colors.primary}
+        onPress={() => navigation.navigate('Medio1Ano')}
+      />
+      <Button
+        title="2° Ano Ensino Médio"
+        color={colors.primary}
+        onPress={() => navigation.navigate('Medio2Ano')}
+      />
+      <Button
+        title="3° Ano Ensino Médio"
+        color={colors.primary}
+        onPress={() => navigation.navigate('Medio3Ano')}
+      />
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.background,
+  },
+});
+
+export default MedioTela;
