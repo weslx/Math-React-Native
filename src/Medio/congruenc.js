23bbc4c7393564e31b2b05b7ae9bed661c565f95
wesley <wesleyaleixo0950@gmail.com>
2023-10-20 17:38:24 -0300

    Lei de seno

--- src/Medio/congruenc.js
+++ src/Medio/congruenc.js
@@ -4,42 +4,91 @@ import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 
 const CongruencCalculator = () => {
   const [ladoA, setLadoA] = useState("");
+  const [ladoB, setLadoB] = useState("");
+  const [ladoC, setLadoC] = useState("");
   const [anguloA, setAnguloA] = useState("");
   const [anguloB, setAnguloB] = useState("");
   const [anguloC, setAnguloC] = useState("");
   const [resultado, setResultado] = useState("");
 
   const calcularLadoLeiDosSenos = () => {
-    // Certifique-se de que os valores de entrada sejam números
-    const a = parseFloat(ladoA);
-    const A = parseFloat(anguloA);
-    const B = parseFloat(anguloB);
-    const C = parseFloat(anguloC);
+    // Certifique-se de que pelo menos um lado e um ângulo sejam fornecidos
+    if ((ladoA || ladoB || ladoC) && (anguloA || anguloB || anguloC)) {
+      // Certifique-se de que os valores de entrada sejam números
+      const A = anguloA ? parseFloat(anguloA) : null;
+      const B = anguloB ? parseFloat(anguloB) : null;
+      const C = anguloC ? parseFloat(anguloC) : null;
+      const a = ladoA ? parseFloat(ladoA) : null;
+      const b = ladoB ? parseFloat(ladoB) : null;
+      const c = ladoC ? parseFloat(ladoC) : null;
 
-    if (!isNaN(a) && !isNaN(A) && !isNaN(B) && !isNaN(C)) {
-      // Certifique-se de que os ângulos estão em radianos
-      const radianosA = (A * Math.PI) / 180;
-      const radianosB = (B * Math.PI) / 180;
-      const radianosC = (C * Math.PI) / 180;
-
-      // Usando a Lei dos Senos: (a / sen(A)) = (b / sen(B)) = (c / sen(C))
-      const ladoB = (a * Math.sin(radianosB)) / Math.sin(radianosA);
-      const ladoC = (a * Math.sin(radianosC)) / Math.sin(radianosA);
-
-      setResultado(`Lado B: ${ladoB.toFixed(2)}, Lado C: ${ladoC.toFixed(2)}`);
+      if (
+        (A === null || !isNaN(A)) &&
+        (B === null || !isNaN(B)) &&
+        (C === null || !isNaN(C)) &&
+        (a === null || !isNaN(a)) &&
+        (b === null || !isNaN(b)) &&
+        (c === null || !isNaN(c))
+      ) {
+        // Certifique-se de que pelo menos um ângulo seja diferente de zero
+        if (A !== 0 || B !== 0 || C !== 0) {
+          // Verifique quais lados foram fornecidos e calcule o lado ausente
+          if (a && A) {
+            if (B) {
+              const radianosA = (A * Math.PI) / 180;
+              const radianosB = (B * Math.PI) / 180;
+              const ladoB = (a * Math.sin(radianosB)) / Math.sin(radianosA);
+              setResultado(`Lado B: ${ladoB.toFixed(2)}`);
+            } else if (C) {
+              const radianosA = (A * Math.PI) / 180;
+              const radianosC = (C * Math.PI) / 180;
+              const ladoC = (a * Math.sin(radianosC)) / Math.sin(radianosA);
+              setResultado(`Lado C: ${ladoC.toFixed(2)}`);
+            }
+          } else if (b && B) {
+            // Similar para ladoB e anguloB
+          } else if (c && C) {
+            // Similar para ladoC e anguloC
+          } else {
+            setResultado(
+              "Por favor, forneça um lado e um ângulo correspondente."
+            );
+          }
+        } else {
+          setResultado("Ângulos não podem ser iguais a zero.");
+        }
+      } else {
+        setResultado(
+          "Por favor, insira valores válidos para os ângulos e lados."
+        );
+      }
     } else {
-      setResultado("Por favor, insira valores válidos.");
+      setResultado("Por favor, forneça pelo menos um lado e um ângulo.");
     }
   };
 
   return (
     <View style={styles.center}>
-      <Text style={styles.font}>Lei dos Senos Calculator</Text>
+      <Text style={styles.font}>Lei dos Senos</Text>
       <TextInput
         style={styles.inputs}
         placeholder="Lado A"
         onChangeText={(text) => setLadoA(text)}
         value={ladoA}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.inputs}
+        placeholder="Lado B"
+        onChangeText={(text) => setLadoB(text)}
+        value={ladoB}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.inputs}
+        placeholder="Lado C"
+        onChangeText={(text) => setLadoC(text)}
+        value={ladoC}
         keyboardType="numeric"
       />
       <TextInput
