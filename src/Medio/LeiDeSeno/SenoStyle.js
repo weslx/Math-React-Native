d727fa309047085c980618a56f07f9d7a200ce1f
wesley <wesleyaleixo0950@gmail.com>
2023-10-27 17:12:04 -0300

    seno style

--- src/Medio/LeiDeSeno/SenoStyle.js
+++ src/Medio/LeiDeSeno/SenoStyle.js
@@ -17,6 +17,14 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     width: 200,
     height: 50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   font: {
     fontWeight: "bold",
