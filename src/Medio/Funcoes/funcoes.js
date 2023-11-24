ae370bbc0dc582e3f8dab53ae50a4fb3f2f50f4f
wesley <wesleyaleixo0950@gmail.com>
2023-11-24 15:18:48 -0300

    fix quadratica

--- src/Medio/Funcoes/funcoes.js
+++ src/Medio/Funcoes/funcoes.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, ScrollView } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import { LineChart } from "react-native-chart-kit";
 
-const Funções = () => {
+const Funçoes = () => {
   const [selectedFunction, setSelectedFunction] = useState("Função Quadrática");
   const [result, setResult] = useState("");
   const [calculationSteps, setCalculationSteps] = useState("");
@@ -10,8 +11,9 @@ const Funções = () => {
   const [inputValue, setInputValue] = useState("");
   const [coeficienteQuadraticoValue, setCoeficienteQuadraticoValue] =
     useState("");
+  const [coeficienteLinearValue, setCoeficienteLinearValue] = useState("");
   const [constanteQuadraticaValue, setConstanteQuadraticaValue] = useState("");
-  const [chartSeries, setChartSeries] = useState([]);
+  const [chartData, setChartData] = useState({});
 
   const getFormula = (selectedFunction) => {
     if (selectedFunction === "Função Quadrática") {
@@ -32,15 +34,18 @@ const Funções = () => {
 
     if (selectedFunction === "Função Quadrática") {
       let coeficienteQuadratico = parseFloat(coeficienteQuadraticoValue) || 0;
+      let coeficienteLinear = parseFloat(coeficienteLinearValue) || 0;
       let constanteQuadratica = parseFloat(constanteQuadraticaValue) || 0;
       resultValue =
-        coeficienteQuadratico * Math.pow(value, 2) + constanteQuadratica;
+        coeficienteQuadratico * Math.pow(value, 2) +
+        coeficienteLinear * value +
+        constanteQuadratica;
     } else if (selectedFunction === "Função Linear") {
       resultValue = 2 * value + 3;
     } else if (selectedFunction === "Função Exponencial") {
       resultValue = Math.exp(value);
     } else if (selectedFunction === "Função Logarítmica") {
-      // Implemente a lógica para a função logarítmica conforme necessário
+      resultValue = Math.log(value); // Lógica básica para logaritmo natural
     }
 
     let steps = "Passos do cálculo:\n";
@@ -50,23 +55,40 @@ const Funções = () => {
 
     setResult(`Resultado:\n${resultValue}`);
     setCalculationSteps(steps);
-    setChartSeries(
-      Array.from({ length: 21 }, (_, index) => index - 10).map((value) => ({
-        x: value,
-        y:
-          selectedFunction === "Função Quadrática"
-            ? calculateQuadraticFunction(value)
-            : selectedFunction === "Função Linear"
-            ? calculateLinearFunction(value)
-            : calculateExponentialFunction(value),
-      }))
-    );
+    updateChartData(selectedFunction);
+  };
+
+  const updateChartData = (selectedFunction) => {
+    const data = {
+      labels: Array.from({ length: 21 }, (_, index) => index - 10),
+      datasets: [
+        {
+          data: Array.from({ length: 21 }, (_, index) => {
+            const value = index - 10;
+            return selectedFunction === "Função Quadrática"
+              ? calculateQuadraticFunction(value)
+              : selectedFunction === "Função Linear"
+              ? calculateLinearFunction(value)
+              : selectedFunction === "Função Exponencial"
+              ? calculateExponentialFunction(value)
+              : Math.log(value); // Lógica básica para logaritmo natural
+          }),
+        },
+      ],
+    };
+
+    setChartData(data);
   };
 
   const calculateQuadraticFunction = (value) => {
     let coeficienteQuadratico = parseFloat(coeficienteQuadraticoValue) || 0;
+    let coeficienteLinear = parseFloat(coeficienteLinearValue) || 0;
     let constanteQuadratica = parseFloat(constanteQuadraticaValue) || 0;
-    return coeficienteQuadratico * Math.pow(value, 2) + constanteQuadratica;
+    return (
+      coeficienteQuadratico * Math.pow(value, 2) +
+      coeficienteLinear * value +
+      constanteQuadratica
+    );
   };
 
   const calculateLinearFunction = (value) => {
@@ -93,7 +115,7 @@ const Funções = () => {
           <Picker.Item label="Função Quadrática" value="Função Quadrática" />
           <Picker.Item label="Função Linear" value="Função Linear" />
           <Picker.Item label="Função Exponencial" value="Função Exponencial" />
-          {/* Adicione mais Picker.Items conforme necessário */}
+          <Picker.Item label="Função Logarítmica" value="Função Logarítmica" />
         </Picker>
         <Text style={{ fontWeight: "bold", fontSize: 18, marginTop: 20 }}>
           {formula}
@@ -111,6 +133,11 @@ const Funções = () => {
               placeholder="Digite o coeficiente quadrático (a)"
             />
             <TextInput
+              value={coeficienteLinearValue}
+              onChangeText={(text) => setCoeficienteLinearValue(text)}
+              placeholder="Digite o coeficiente linear (b)"
+            />
+            <TextInput
               value={constanteQuadraticaValue}
               onChangeText={(text) => setConstanteQuadraticaValue(text)}
               placeholder="Digite a constante (c)"
@@ -124,9 +151,44 @@ const Funções = () => {
         <Text style={{ fontStyle: "italic", marginTop: 20 }}>
           {calculationSteps}
         </Text>
+        {Object.keys(chartData).length > 0 && (
+          <View style={{ marginTop: 20 }}>
+            <Text
+              style={{ fontWeight: "bold", fontSize: 18, marginBottom: 10 }}
+            >
+              Gráfico da Função:
+            </Text>
+            <LineChart
+              data={chartData}
+              width={300}
+              height={200}
+              chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2,
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                },
+                propsForDots: {
+                  r: "6",
+                  strokeWidth: "2",
+                  stroke: "#ffa726",
+                },
+              }}
+              bezier
+              style={{
+                marginVertical: 8,
+                borderRadius: 16,
+              }}
+            />
+          </View>
+        )}
       </View>
     </ScrollView>
   );
 };
 
-export default Funções;
+export default Funçoes;
